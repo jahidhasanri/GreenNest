645f367ea79eb6cbcbc1d944dd6a042e2faba585
jahid hasan rifat <jahadhasanrifat@gmail.com>
2026-01-03 19:54:31 +0600

    add checkOutOutCategories component

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from './components/Banner';
 import AbouOurService from './components/AbouOurService';
+import CheckOutOurCategories from './components/CheckOutOurCategories';
 
 const Homepage = () => {
   return (
     <div>
       <Banner></Banner>
       <AbouOurService></AbouOurService>
+      <CheckOutOurCategories></CheckOutOurCategories>
     </div>
   );
 };
